6649526f47f09261b0c52f40c0fe073545770b09
Lucas Marques <lucas.silva@dextra-sw.com>
2021-03-22 21:20:31 -0300

    Update storybook and remove deprecated packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,4 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.tsx"
-  ],
-  "addons": [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-knobs/register"
-  ]
+  "stories": ["../src/components/**/stories.tsx"],
+  "addons": ["@storybook/addon-essentials"]
 }
